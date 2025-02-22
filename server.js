94ca5ca09d24c2441191ca0114445bf270e183c9
Muhammad Umar <muhammad-umar@umarabdulhaque@gmail.com>
2025-02-22 05:13:47 +0500

    remove slash

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 app.use(
     cors({
-      origin: "https://gym-dashboard-site.netlify.app/", // Allow frontend URL
+      origin: "https://gym-dashboard-site.netlify.app", // Allow frontend URL
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true, // Allow cookies & authentication headers
